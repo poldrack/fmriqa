ae3f2e8cd52c11f5816a5a3bbadc148b6d57955c
Russ Poldrack <poldrack@gmail.com>
2013-03-21 13:48:37 -0500

    fixed indexing problems for non-axial data

--- mk_slice_mosaic.py
+++ mk_slice_mosaic.py
@@ -10,24 +10,42 @@ import matplotlib.pyplot as plt
 import sys
 
 
-def mk_slice_mosaic(imgdata,outfile,title,contourdata=[],ncols=6,colorbar=True,dir='axial'):
+def mk_slice_mosaic(imgdata,outfile,title,contourdata=[],ncols=6,colorbar=True):
+    if imgdata.shape[0]==imgdata.shape[1]==imgdata.shape[2]:
+        min_dim=2
+    else:
+        min_dim=N.where(N.min(imgdata.shape[0:3])==imgdata.shape[0:3])[0]
 
+        slice_dims=N.where(N.min(imgdata.shape[0:3])!=imgdata.shape[0:3])[0]
+        print 'min_dim:',min_dim
+        print 'slice_dim:',slice_dims
+        
     #if not len(imgdata.shape)==3:
     #    sys.stdout.write(__doc__)
-    if dir=='axial':
-        nrows=int(N.ceil(N.float(imgdata.shape[2])/ncols))
-        mosaic=N.zeros((nrows*imgdata.shape[0],ncols*imgdata.shape[1]))
+    if 1:
+        nrows=int(N.ceil(N.float(imgdata.shape[min_dim])/ncols))
+        mosaic=N.zeros((nrows*imgdata.shape[slice_dims[0]],ncols*imgdata.shape[slice_dims[1]]))
         if not contourdata==[]:
-            contourmosaic=N.zeros((nrows*imgdata.shape[0],ncols*imgdata.shape[1]))
+            contourmosaic=N.zeros((nrows*imgdata.shape[slice_dims[0]],ncols*imgdata.shape[slice_dims[1]]))
         ctr=0
+        print mosaic.shape
+        
         for row in range(nrows):
-            rowstart=row*imgdata.shape[0]
-            rowend=(row+1)*imgdata.shape[0]
+            rowstart=row*imgdata.shape[slice_dims[0]]
+            rowend=(row+1)*imgdata.shape[slice_dims[0]]
             for col in range(ncols):
-                if ctr<imgdata.shape[2]:
-                    colstart=col*imgdata.shape[1]
-                    colend=(col+1)*imgdata.shape[1]
-                    mosaic[rowstart:rowend,colstart:colend]=imgdata[:,::-1,ctr].T
+                if ctr<imgdata.shape[min_dim]:
+                    colstart=col*imgdata.shape[slice_dims[1]]
+                    colend=(col+1)*imgdata.shape[slice_dims[1]]
+                    print rowstart,rowend,colstart,colend
+                    if min_dim==0:
+                        imgslice=imgdata[ctr,:,::-1].T
+                    elif min_dim==1:
+                        imgslice=imgdata[:,ctr,::-1].T
+                    elif min_dim==2:
+                        imgslice=imgdata[:,::-1,ctr].T
+                    print imgslice.shape
+                    mosaic[rowstart:rowend,colstart:colend]=imgslice
                     if not contourdata==[]:
                         contourmosaic[rowstart:rowend,colstart:colend]=contourdata[:,::-1,ctr].T
                     ctr+=1
